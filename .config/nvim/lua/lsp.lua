adf91c87748a638106fc65500c5ca27e43d6482b
frahz <abreufranklin589@gmail.com>
2022-10-23 21:51:30 -0700

    Add sumneko_lua to lsp file

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,6 +1,11 @@
 
 local nvim_lsp = require("lspconfig")
 
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+
 local on_attach = function(client)
     -- Don't know what this does ??
     -- require("cmp").on_attach(client)
@@ -43,3 +48,25 @@ nvim_lsp.rust_analyzer.setup({
         }
     }
 })
+
+nvim_lsp.sumneko_lua.setup({
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                globals = { "vim" },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+})
+
+
