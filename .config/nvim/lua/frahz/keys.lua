68d6eaab6e3a6968ae4eb2e4388aed85e3fb988a
frahz <abreufranklin589@gmail.com>
2022-11-12 21:56:40 -0800

    Add initial settings for Telescope
    - keys.lua: change prefix from ":" to "<cmd>"

--- .config/nvim/lua/frahz/keys.lua
+++ .config/nvim/lua/frahz/keys.lua
@@ -1,6 +1,6 @@
 -- function wrapper for mapping custom keybinds
 local map = function(mode, lhs, rhs, opts)
-    local options = {noremap = true, silent = true}
+    local options = { noremap = true, silent = true }
     if opts then
         options = vim.tbl_extend("force", options, opts)
     end
@@ -11,8 +11,16 @@ end
 vim.g.mapleader = " " -- space
 
 -- keybinds for moving a line up or down
-map("n", "<S-Up>", ":m-2<CR>")
-map("n", "<S-Down>", ":m+<CR>")
-map("i", "<S-Up>", "<Esc>:m-2<CR>")
-map("i", "<S-Down>", "<Esc>:m+<CR>")
+map("n", "<S-Up>", "<cmd>m-2<CR>")
+map("n", "<S-Down>", "<cmd>m+<CR>")
+map("i", "<S-Up>", "<Esc><cmd>m-2<CR>")
+map("i", "<S-Down>", "<Esc><cmd>m+<CR>")
 
+-- Telescope
+map("n", "<Leader>ff", "<cmd>Telescope find_files<CR>")
+map("n", "<Leader>fg", "<cmd>Telescope git_files<CR>")
+map("n", "<Leader>fw", "<cmd>Telescope live_grep<CR>")
+map("n", "<Leader>fh", "<cmd>Telescope buffers<CR>")
+map("n", "<Leader>fo", "<cmd>Telescope oldfiles<CR>")
+map("n", "<Leader>fc", "<cmd>Telescope git_commits<CR>")
+map("n", "<Leader>fd", "<cmd>Telescope diagnostics<CR>")
