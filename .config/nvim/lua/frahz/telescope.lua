dcc9ad42c8f486522da04acc5ec5c61aaf81e26f
frahz <abreufranklin589@gmail.com>
2023-09-17 18:36:46 -0700

    nvim: change telescope layout

--- .config/nvim/lua/frahz/telescope.lua
+++ .config/nvim/lua/frahz/telescope.lua
@@ -4,6 +4,13 @@ local actions = require("telescope.actions")
 
 telescope.setup({
     defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.65,
+            },
+        },
     },
     extensions = {
         fzf = {
